a3fde7684002acf145252d7f9e585269b72da0c6
Anushiya <anushiya1509@gmail.com>
2020-09-25 15:46:29 +0530

    Rectangle

---
+++ NodeJS/NodeExamples/index.js
@@ -0,0 +1,24 @@
+var rectangle = {
+
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+
+};
+
+function solve(l,b) {
+
+    console.log(`Length = ${l} Breadth = ${b}`);
+
+    if(l<=0 || b<=0)
+        console.log("Length and breadth should be greater than 0");
+    else{
+        console.log("Area of the rectangle is " + rectangle.area(l,b));
+        console.log("Perimeter of the rectangle is "+ rectangle.perimeter(l,b));
+    }
+}
+
+solve(-1,1);
+solve(0,1);
+solve(1,-1);
+solve(1,0);
+solve(14,15);
