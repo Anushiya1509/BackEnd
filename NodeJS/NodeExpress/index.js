19b9529a423c0186137b86e65be71beb7a2f840e
Anushiya <anushiya1509@gmail.com>
2020-09-26 15:59:43 +0530

    Express-1

---
+++ NodeJS/NodeExpress/index.js
@@ -0,0 +1,17 @@
+const { text } = require('express');
+const express = require('express');
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+const app = express();
+app.use((req,res)=>{
+    console.log(req.headers);
+    req.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.end('<html><body><h1>Express</h1></body></html>');
+});
+
+const server = http.createServer(app);
+server.listen(port, hostname, ()=>{
+    console.log(`Server running at http://${hostname}:${port}`);
+})
