b8a5b11f2a09118159e4de9d05a1cf9dbf0a136c
Anushiya <anushiya1509@gmail.com>
2020-09-26 16:06:35 +0530

    Express morgan, express.static

--- NodeJS/NodeExpress/index.js
+++ NodeJS/NodeExpress/index.js
@@ -1,11 +1,17 @@
 const { text } = require('express');
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+
 const hostname = 'localhost';
 const port = 3000;
+
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + "/public"));
+
 app.use((req,res)=>{
-    console.log(req.headers);
     req.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
     res.end('<html><body><h1>Express</h1></body></html>');
